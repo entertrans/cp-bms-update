393b4f8baf104535b8587ce9d9ccd2783d9691ea
Imam Maulana Ibrahim <imamaulana26.github@gmail.com>
2020-06-22 20:59:27 +0700

    perbaikan UI/UX project

--- application/views/home.php
+++ application/views/home.php
@@ -1,7 +1,7 @@
 <!-- Slider -->
 <div id="main-slider" class="dl-slider">
     <?php foreach ($slider as $dt) {
-        ?>
+    ?>
         <div class="single-slide">
             <div class="bg-img kenburns-top-right" style="background-image: url(<?= base_url('assets/mockup/core/img/slider/') . $dt['photo_slider'] ?>);background-size: cover;background-position: center;"></div>
             <div class="overlay"></div>
@@ -19,30 +19,31 @@
                             </div>
                         </div>
 
-<!--                     <div class="dl-caption big">
+                        <!--                     <div class="dl-caption big">
                         <div class="inner-layer">
                             <div data-animation="fade-in-left" data-delay="2.5s">construction services.</div>
                         </div>
                     </div> -->
 
-                    <div class="dl-caption small">
-                        <div class="inner-layer">
-                            <div data-animation="fade-in-left" data-delay="3s"><?= $dt['desc_slider']  ?></div>
+                        <div class="dl-caption small">
+                            <div class="inner-layer">
+                                <div data-animation="fade-in-left" data-delay="3s"><?= $dt['desc_slider']  ?></div>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="dl-btn-group">
-                        <div class="inner-layer">
-                            <a href="<?= site_url('project') ?>" class="dl-btn" data-animation="fade-in-left" data-delay="3.5s">View Projects <i class="arrow_right"></i></a>
+                        <div class="dl-btn-group">
+                            <div class="inner-layer">
+                                <a href="<?= site_url('project') ?>" class="dl-btn" data-animation="fade-in-left" data-delay="3.5s">View Projects <i class="arrow_right"></i></a>
+                            </div>
                         </div>
-                    </div>
 
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-<?php }  ?>
-<!--     <div class="single-slide">
+    <?php }  ?>
+
+    <!--     <div class="single-slide">
         <div class="bg-img kenburns-top-right" style="background-image: url(<?= base_url('assets/mockup/core/img/slider-2.jpg') ?>);"></div>
         <div class="overlay"></div>
         <div class="slider-content-wrap d-flex align-items-center text-center">
@@ -120,7 +121,6 @@
 <section class="blog-section padding">
     <div class="container">
         <div class="section-heading text-center mb-40 wow fadeInUp" data-wow-delay="100ms">
-            <!-- <span>From Blog</span> -->
             <h2>Material</h2>
         </div>
         <div class="row blog-wrap">
--- application/views/project.php
+++ application/views/project.php
@@ -1,85 +1,84 @@
-        <section class="page-header padding">
-        	<div class="container">
-        		<div class="page-content text-center">
-        			<h2>DOKUMENTASI</h2>
-        			<p>ini adalah Beberapa Project yang telah selesai</p>
-        		</div>
-        	</div>
-        </section>
-        <style>
-          .modal-footer{
-            padding-top: 0px;
-          }
-          .modal-content{
-            margin-top: 50px;
-          }
-        </style>
-        <section class="projects-section padding">
-         <div class="container">
+<section class="page-header padding">
+    <div class="container">
+        <div class="page-content text-center">
+            <h2>DOKUMENTASI</h2>
+            <p>ini adalah Beberapa Project yang telah selesai</p>
+        </div>
+    </div>
+</section>
+<style>
+    .modal-footer {
+        padding-top: 0px;
+    }
 
-          <div class="row">
+    .modal-content {
+        margin-top: 50px;
+    }
+
+    .project-item {
+        border-radius: 10px 10px 10px 10px;
+        -moz-border-radius: 10px 10px 10px 10px;
+        -webkit-border-radius: 10px 10px 10px 10px;
+        border: 0px solid #000000;
+    }
+
+    .project-item:hover {
+        -webkit-box-shadow: 10px 10px 15px -5px rgba(0, 0, 0, 0.5);
+        -moz-box-shadow: 10px 10px 15px -5px rgba(0, 0, 0, 0.5);
+        box-shadow: 10px 10px 15px -5px rgba(0, 0, 0, 0.5);
+    }
+</style>
+<section class="projects-section padding">
+    <div class="container">
+
+        <div class="row">
             <?php foreach ($dokumentasi as $dt) {
-             ?>
-             <div class="col-lg-4 col-sm-6 padding-15 dokBox moreBox" style="display: none;">
-              <div class="project-item">
-                <div class="col-md">
-                  <div class="row">
-                    <!-- src="<?= base_url('assets/mockup/core/img/slider/') . $dt['photo_slider'] ?>" -->
-                    <!-- <?= base_url('assets/mockup/core/img/dokumentasi/') . $dt['photo_dok'] ?> -->
-                    <a href="<?= base_url('assets/mockup/core/img/dokumentasi/') . $dt['photo_dok'] ?>" data-toggle="lightbox" data-title="<?= $dt['pr_title'] ?>" data-footer="<?= $dt['pr_desc'] ?>">
-                      <img style="background-image: url(<?= base_url('assets/mockup/core/img/dokumentasi/') . $dt['photo_dok'] ?>);background-size: cover;
+            ?>
+                <div class="col-lg-4 col-sm-6 padding-15 dokBox moreBox" style="display: none;">
+                    <div class="project-item">
+                        <div class="row">
+                            <div class="col-md">
+                                <a href="<?= base_url('assets/mockup/core/img/dokumentasi/') . $dt['photo_dok'] ?>" data-toggle="lightbox" data-title="<?= $dt['pr_title'] ?>" data-footer="<?= $dt['pr_desc'] ?>">
+                                    <img style="background-image: url(<?= base_url('assets/mockup/core/img/dokumentasi/') . $dt['photo_dok'] ?>);background-size: cover;
                       background-repeat: no-repeat;
                       background-position: center;" src="<?= base_url('assets/mockup/core/img/frame-kosong.png')  ?>" class="img-fluid" alt="projects">
-                    </a>
-                  </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="projects-content">
+                            <h3 class="category">Project PT.abc</h3>
+                            <h3 class="tittle" style="text-decoration: none;">Kepulauan Seribu</h3>
+                        </div>
+                    </div>
                 </div>
-                <div class="projects-content">
-                  <a href="#" class="category">Project PT.abc</a>
-                  <h3><a href="#" class="tittle">Kepulauan Seribu</a></h3>
-                </div>
-              </div>
-            </div>
-          <?php } ?>
+            <?php } ?>
         </div>
-      </div>
-      <div class="section-heading text-center mt-40 wow fadeInUp" data-wow-delay="100ms">
+    </div>
+    <div class="section-heading text-center mt-40 wow fadeInUp" data-wow-delay="100ms">
         <button id="loadMore" type="button" class="btn btn-outline-primary btn-lg">View More</button>
-      </div>
-    </section>
-    <?php $this->load->view('mockup/layout/core/js'); ?>
+    </div>
+</section>
+<?php $this->load->view('mockup/layout/core/js'); ?>
 
-    <script>
-      $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+<script>
+    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
         event.preventDefault();
         $(this).ekkoLightbox();
 
-      });
-    </script>
-    <script>
-      $( document ).ready(function () {
+    });
+</script>
+<script>
+    $(document).ready(function() {
         $(".moreBox").slice(0, 6).show();
         if ($(".dokBox:hidden").length != 0) {
-          $("#loadMore").show();
-        }   
-        $("#loadMore").on('click', function (e) {
-          e.preventDefault();
-          $(".moreBox:hidden").slice(0, 6).slideDown();
-          if ($(".moreBox:hidden").length == 0) {
-            $("#loadMore").fadeOut('slow');
-          }
+            $("#loadMore").show();
+        }
+        $("#loadMore").on('click', function(e) {
+            e.preventDefault();
+            $(".moreBox:hidden").slice(0, 6).slideDown();
+            if ($(".moreBox:hidden").length == 0) {
+                $("#loadMore").fadeOut('slow');
+            }
         });
-      });
-    </script>
-
-    <!-- backup -->
-<!-- <div class="col-lg-4 col-sm-6 padding-15">
-        <div class="project-item">
-         <img src="<?= base_url('assets/mockup/core/img/project-3.jpg') ?>" alt="projects">
-         <div class="overlay"></div>
-         <a href="<?= base_url('assets/mockup/core/img/project-3.jpg') ?>" class="view-icon  img-popup"> <i class="fas fa-expand"></i></a>
-         <div class="projects-content">
-          <a href="#" class="category">Project PT.abc</a>
-          <h3><a href="#" class="tittle">Kepulauan Seribu</a></h3>
-        </div>
-      </div>
-    </div> -->
+    });
+</script>
